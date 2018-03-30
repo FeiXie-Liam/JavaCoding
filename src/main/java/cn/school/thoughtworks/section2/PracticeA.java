4c84a2d7f9c03ad24860650cb1a53e093c7d0878
FeiXie-Liam <xiefei0887@163.com>
2018-03-30 19:33:13 +0800

    finish second section!

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -2,11 +2,20 @@ package cn.school.thoughtworks.section2;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String,Integer> result = new HashMap<>();
 
-        return null;
+        for (int i = 0; i < collection1.size(); i++) {
+            String element = collection1.get(i);
+            if (result.containsKey(element)) {
+                result.replace(element, result.get(element) + 1);
+            }
+            else
+                result.put(element, 1);
+        }
+        return result;
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -2,11 +2,47 @@ package cn.school.thoughtworks.section2;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 public class PracticeB {
-    Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+    public Map<String, Integer> put(Map<String, Integer> map, String str, int num) {
+        if (map.containsKey(str)) {
+            map.replace(str, map.get(str) + num);
+        } else
+            map.put(str, num);
+        return map;
+    }
 
-        return null;
+    public Map<String, Integer> put(Map<String, Integer> map, String str) {
+        if (map.containsKey(str)) {
+            map.replace(str, map.get(str) + 1);
+        } else
+            map.put(str, 1);
+        return map;
+    }
+
+    Map<String, Integer> countSameElements(List<String> collection1) {
+        Map<String, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < collection1.size(); i++) {
+            String element = collection1.get(i);
+            if (element.length() != 1) {
+                if (element.contains("-")) {
+                    int num = Integer.parseInt(element.substring(element.indexOf("-") + 1));
+                    result = put(result, element.substring(0, 1), num);
+                } else if (element.contains(":")) {
+                    int num = Integer.parseInt(element.substring(element.indexOf(":") + 1));
+                    result = put(result, element.substring(0, 1), num);
+                } else if (element.contains("[")) {
+                    int num = Integer.parseInt(element.substring(element.indexOf("[") + 1, element.indexOf("]")));
+                    result = put(result, element.substring(0, 1), num);
+                }
+            }
+            else {
+                result = put(result, element);
+            }
+
+        }
+        return result;
     }
 }
